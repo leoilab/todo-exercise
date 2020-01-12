6c3a609191cfd5fa5c72bf3b484ede79ea064a99
erdeszt <erdeszt@gmail.com>
2020-01-12 21:51:48 +0100

    Trx with tagless

--- src/main/scala/todo/TodoService.scala
+++ src/main/scala/todo/TodoService.scala
@@ -7,7 +7,13 @@ import shapeless.{:+:, CNil, Coproduct}
 import todo.Model.Todo
 import cats.syntax.apply._
 import cats.syntax.flatMap._
+import cats.syntax.functor._
+import doobie.implicits._
+import doobie.free.connection.{ConnectionIO, ConnectionOp}
+import doobie.util.transactor.Transactor
 import todo.Errors.{DomainError, TodoNotFound}
+
+import scala.concurrent.ExecutionContext
 
 object Errors {
   sealed abstract class DomainError(message: String) extends Throwable(message)
@@ -109,6 +115,87 @@ object TodoServiceF {
         case _ => Right(Unit)
       }
     }
+  }
+
+}
+
+object TaglessTrx {
+
+  type Trx[A] = ConnectionIO[A]
+
+  trait TodoStore[F[_]] {
+    def list: F[Vector[Todo]]
+    def create(name: String): Trx[Unit]
+    def finish(id:   Int):    Trx[UpdateResult]
+  }
+
+  class TodoStoreImplForIO(transactor: Transactor[IO]) extends TodoStore[IO] {
+    def list: IO[Vector[Todo]] = {
+      sql"select id, name, done from todo".query[Todo].to[Vector].transact(transactor)
+    }
+
+    def create(name: String): Trx[Unit] = {
+      val statement = sql"insert into todo (name, done) values (${name}, 0)"
+      statement.update.run
+        .flatMap {
+          case 0       => MonadError[ConnectionIO, Throwable].raiseError(FailedToInsert(statement))
+          case 1       => Monad[ConnectionIO].pure(())
+          case results => MonadError[ConnectionIO, Throwable].raiseError(TooManyWriteResults(statement, results))
+        }
+    }
+
+    def finish(id: Int): Trx[UpdateResult] = {
+      MonadError[ConnectionIO, Throwable].raiseError[UpdateResult](new Exception("STOP"))
+    }
+
+    def finish_Working(id: Int): Trx[UpdateResult] = {
+      val statement = sql"update todo set done = 1 where id = ${id}"
+      statement.update.run.flatMap {
+        case 0       => Monad[ConnectionIO].pure(UpdateResult.NotFound)
+        case 1       => Monad[ConnectionIO].pure(UpdateResult.Updated)
+        case results => MonadError[ConnectionIO, Throwable].raiseError(TooManyWriteResults(statement, results))
+      }
+    }
+  }
+
+  trait TrxHandler[F[_]] {
+    def trx[A](action: Trx[A]): F[A]
+  }
+
+  class DoobieTrxHandler(transactor: Transactor[IO]) extends TrxHandler[IO] {
+    def trx[A](action: Trx[A]): IO[A] = {
+      action.transact(transactor)
+    }
+  }
+
+  def program[F[_]: Monad](implicit store: TodoStore[F], trx: TrxHandler[F], logger: LoggerF[F]): F[Unit] = {
+    for {
+      _ <- logger.logInfo("Tagless transaction test")
+      _ <- trx.trx {
+        for {
+          _ <- store.create("failed")
+          _ <- store.finish(1)
+        } yield ()
+      }
+      _ <- logger.logInfo("Success")
+    } yield ()
+  }
+
+  def main(args: Array[String]): Unit = {
+    implicit val cs = IO.contextShift(ExecutionContext.global)
+    val transactor = Transactor.fromDriverManager[IO](
+      "org.sqlite.JDBC",
+      "jdbc:sqlite:todo.db"
+    )
+    implicit val logger = new LoggerFImplForIO
+    // You can use a readonly connection for the store
+    // or configure different thread pools for read/write transactions
+    implicit val store      = new TodoStoreImplForIO(transactor)
+    implicit val trxHandler = new DoobieTrxHandler(transactor)
+
+    program[IO].unsafeRunSync()
+
+    ()
   }
 
 }
